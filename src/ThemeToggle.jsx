471403dae0000091706599dd505bd3ef55426c2c
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-28 14:20:18 +0900

    테마 토글 완료

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
+import { useGlobalContext } from "./context";
 
 const ThemeToggle = () => {
-  return <div>ThemeToggle</div>;
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillMoonFill className="toggle-icon " style={{ color: "white" }} />
+        ) : (
+          <BsFillSunFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
 };
 
 export default ThemeToggle;
