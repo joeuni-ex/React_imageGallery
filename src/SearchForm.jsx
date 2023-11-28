fa48bd8e9337b7ab3bb69473015bf76e3ed52fff
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-28 15:12:48 +0900

    검색어 입력

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -4,9 +4,11 @@ const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
   const handleSubmit = (e) => {
     e.preventDefault();
-    const searchValue = e.target.elements.search.value;
-    if (!searchValue) return;
-    setSearchTerm(searchValue);
+    const searchValue = e.target.elements.search.value?.trim();
+
+    if (searchValue !== null && searchValue.length > 1) {
+      setSearchTerm(searchValue); //검색어를 공통으로 저장한다.
+    }
   };
   return (
     <section>
